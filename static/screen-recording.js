58437f18bf5f59673158cb783b2a8fd803743fc4
GuoYipeng <yg2719@ic.ac.uk>
2023-09-14 20:52:06 +0100

    live

--- static/screen-recording.js
+++ static/screen-recording.js
@@ -3,32 +3,41 @@ console.log('JavaScript file loaded');
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
 const recordedVideo = document.getElementById('recordedVideo');
+const liveVideo = document.getElementById('liveVideo'); // Add an element for live video
 
 let mediaRecorder;
 let recordedChunks = [];
 
 startButton.addEventListener('click', async () => {
-    console.log('Button clicked'); // Add this line
-    const stream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+    console.log('Button clicked');
+    
+    try {
+        const stream = await navigator.mediaDevices.getDisplayMedia({ video: { cursor: 'always' } }); // Record the entire screen
 
-    mediaRecorder = new MediaRecorder(stream);
+        // Display the live video stream
+        liveVideo.srcObject = stream;
+        
+        mediaRecorder = new MediaRecorder(stream);
 
-    mediaRecorder.ondataavailable = (e) => {
-        if (e.data.size > 0) {
-            recordedChunks.push(e.data);
-        }
-    };
+        mediaRecorder.ondataavailable = (e) => {
+            if (e.data.size > 0) {
+                recordedChunks.push(e.data);
+            }
+        };
 
-    mediaRecorder.onstop = () => {
-        const blob = new Blob(recordedChunks, { type: 'video/webm' });
-        recordedChunks = [];
-        const url = window.URL.createObjectURL(blob);
-        recordedVideo.src = url;
-    };
+        mediaRecorder.onstop = () => {
+            const blob = new Blob(recordedChunks, { type: 'video/webm' });
+            recordedChunks = [];
+            const url = window.URL.createObjectURL(blob);
+            recordedVideo.src = url;
+        };
 
-    mediaRecorder.start();
-    startButton.disabled = true;
-    stopButton.disabled = false;
+        mediaRecorder.start();
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    } catch (error) {
+        console.error('Error accessing screen recording:', error);
+    }
 });
 
 stopButton.addEventListener('click', () => {
